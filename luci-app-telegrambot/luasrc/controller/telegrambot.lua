cb5f1b218833cb666256cf2cdd1d429b894e88fc
poetra eble5 <83281825+geresa@users.noreply.github.com>
2021-06-13 01:39:50 +0700

    Delete luci-app-telegrambot directory

--- luci-app-telegrambot/luasrc/controller/telegrambot.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.telegrambot", package.seeall)
-
-function index()
-        if not nixio.fs.access("/etc/config/telegrambot") then
-                return
-        end
-        local page
-        page = entry({"admin", "services", "telegrambot"}, cbi("telegrambot"), _("TelegramBot"), 82)
-end
